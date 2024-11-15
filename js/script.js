74eaa5d95946bc6ebceb17f776abcb45ba63ffca
teri.c <teri.charles23@gmail.com>
2024-11-15 14:58:48 -0400

    Created functions to display guessed letters update word in progress and to check if the player won

--- js/script.js
+++ js/script.js
@@ -74,5 +74,46 @@ const makeGuess = function (letter) {
     } else {
         guessedLetters.push(letter);
         console.log(guessedLetters);
+
+        showGuessedLetters();
+        updateWordInProgress(guessedLetters);
+    }
+};
+
+// Function to show the guessed letters
+const showGuessedLetters = function () {
+    guessedLettersElement.innerHTML = "";
+
+    for (const letter of guessedLetters) {
+        const li = document.createElement("li");
+        li.innerText = letter;
+        guessedLettersElement.append(li);
+    }
+};
+
+// Function to update word in progress
+const updateWordInProgress = function (guessedLetters) {
+    const wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    //console.log(wordArray);
+    const revealWord = [];
+
+    for (const letter of wordArray) {
+        if (guessedLetters.includes(letter)) {
+            revealWord.push(letter.toUpperCase());
+        } else {
+        revealWord.push("●");
+        }
+    }
+     wordInProgress.innerText = revealWord.join("");
+    
+     playerWon();
+};
+
+// Function to check is player won
+const playerWon = function () {
+    if (word.toUpperCase() === wordInProgress.innerText) {
+        messages.classList.add("win");
+        messages.innerHTML = `<p class="highlight">You guessed correct the word! Congrats!</p>`;
     }
 };
