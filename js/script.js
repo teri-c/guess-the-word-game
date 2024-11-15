ef15e360de00b3adbda2bd8659cdcbd6b416e005
teri.c <teri.charles23@gmail.com>
2024-11-15 12:16:36 -0400

    Created functions to validate and capture player input fixed variable names and added to button click event

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 // GLOBAL VARIABLES
 // List where the player's guessed letters appear
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersElement = document.querySelector(".guessed-letters");
 // Guess button
 const button = document.querySelector(".guess");
 // Input where player will guess a letter
-const input = document.querySelector(".letter");
+const letterInput = document.querySelector(".letter");
 // Paragraph for word in progress to appear
 const wordInProgress = document.querySelector(".word-in-progress");
 // Paragraph where remaining guesses will display
@@ -17,6 +17,8 @@ const messages = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 // Starting word to test out game
 const word = "magnolia";
+// Array to contain all the letters guessed
+const guessedLetters = [];
 
 // Function to add placeholder's for each letter
 const letterPlaceholders = function (word) {
@@ -35,7 +37,42 @@ letterPlaceholders(word);
 button.addEventListener("click", function (e) {
     e.preventDefault();
 
-    const captureInput = input.value;
-    console.log(captureInput);
-    input.value = "";
+    messages.innerText = "";
+
+    const guess = letterInput.value;
+    
+    const goodGuess = validateInput(guess);
+
+    if (goodGuess) {
+    makeGuess(guess);
+    }
+
+    letterInput.value = "";
 });
+
+// Function to check player's input
+const validateInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+
+    if (input.length === 0) {
+        messages.innerText = `Please enter a letter.`;
+    } else if (input.length > 1) {
+        messages.innerText = `Please enter a single letter.`;
+    } else if (!input.match(acceptedLetter)) {
+        messages.innerText = `Please enter a letter from A to Z.`;
+    } else {
+        return input;
+    }
+};
+
+// Function to capture input
+const makeGuess = function (letter) {
+    letter = letter.toUpperCase();
+
+    if (guessedLetters.includes(letter)) {
+        messages.innerText = `You already guessed that letter, silly. Try again.`;
+    } else {
+        guessedLetters.push(letter);
+        console.log(guessedLetters);
+    }
+};
